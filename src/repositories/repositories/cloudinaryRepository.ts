d899ecca0921af6415d317232937812f55910b89
Bira Botto <tibira.botto@gmail.com>
2025-04-28 18:05:12 -0400

    feat: Add Cloudinary repository for image upload

---
+++ src/repositories/repositories/cloudinaryRepository.ts
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export class CloudinaryRepository {
+  async uploadImage(buffer: Buffer): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { resource_type: "image" },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result!.secure_url);
+        }
+      );
+      uploadStream.end(buffer);
+    });
+  }
+}
